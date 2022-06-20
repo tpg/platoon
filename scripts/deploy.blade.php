8565fb7a93f75bd00ad9762938a3771a6e1fc905
Warrick <me@warrickbayman.com>
2022-06-20 21:57:52 +0200

    wip

--- scripts/deploy.blade.php
+++ scripts/deploy.blade.php
@@ -74,12 +74,24 @@ repo and link the .env file and storage directory.
 -------------------------------------------------------------------}}
 @task('dependencies', ['on' => 'live'])
 
-cd {{ $target->paths('releases', $release) }}
-if [[ ! -f "{{ $target->composer() }}" ]]
+if [[ ! -f "{{ $target->composer }}" ]]
 then
-    curl -sS https://getcomposer.org/installer | {{ $target->php }}
+    cd {{ $target->path }}
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    {{ $target->php }} "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    {{ $target->php }} composer-setup.php --quiet --install-dir={{ $target->composer }}
 fi
 
+cd {{ $target->paths('releases', $release) }}
 {{ $target->composer() }} self-update
 {{ $target->composer() }} install --prefer-dist --no-dev --no-progress --optimize-autoloader
 
