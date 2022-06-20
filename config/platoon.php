fc63306f6609e35481123d59c2e14a882a92f8ce
Warrick <me@warrickbayman.com>
2022-06-20 17:14:14 +0200

    wip

--- config/platoon.php
+++ config/platoon.php
@@ -2,10 +2,39 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default target
+    |--------------------------------------------------------------------------
+    |
+    | The default target if you have more than one deployment target.
+    | This default will be used if no targets are specified during deployment.
+    |
+    */
+
     'default' => 'staging',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Git repository
+    |--------------------------------------------------------------------------
+    |
+    | The Git repository to clone from. You'll need to make sure that you
+    | can clone this repo to all targets before deploying.
+    |
+    */
 
     'repo' => 'repo.git',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Deployment targets
+    |--------------------------------------------------------------------------
+    |
+    | This is the list of deployment targets. You can specify the target
+    | you want to deploy to when using the "platoon:deploy" artisan command.
+    |
+    */
     'targets' => [
 
         'staging' => [
